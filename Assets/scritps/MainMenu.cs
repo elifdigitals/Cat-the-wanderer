b6fc97f53e90e41c3ddc768211535486629ba053
Adilkan <adilkananarbekov751@gmail.com>
2025-12-06 09:02:50 +0600

    MainMenu

---
+++ Assets/scritps/MainMenu.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    // Update is called once per frame
+    public void ExitGame()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+}
